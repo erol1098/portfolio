0a90c273ac2aaa82a949428e8258712dd3b42d47
erol1098 <erol1098@gmail.com>
2022-09-20 18:49:28 +0300

    Project Card Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import Header from './components/Header';
 import Home from './pages/Home';
 
 const App = () => {
-  return <Home />;
+  return (
+    <main className='container-fluid'>
+      <Header />
+      <Home />
+    </main>
+  );
 };
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import Header from '../components/Header';
 
-const Home = () => {
-  return <Header />;
-};
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import ProjectCard from '../components/ProjectCard';
+
+function Home() {
+  return (
+    <Row xs={1} md={2} lg={3} className='g-3'>
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col>
+          <ProjectCard />
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
 export default Home;
