739a8c965ec0f2f8c03d948f1fc915ef8443ab8e
erol1098 <erol1098@gmail.com>
2022-09-20 19:00:50 +0300

    Firebase Hook Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import ProjectCard from '../components/ProjectCard';
+import useFirebase from '../hooks/useFirebase';
 
 function Home() {
+  const [projects, setProjects] = useState([]);
+
+  const { getEntries } = useFirebase();
+  useEffect(() => {
+    getEntries('projects').then((res) => setProjects(res));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log('first', projects);
   return (
     <Row xs={1} md={2} lg={3} className='g-3'>
       {Array.from({ length: 4 }).map((_, idx) => (
